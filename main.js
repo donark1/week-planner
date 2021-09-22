ba44e35ce176182c145a18244dc5abdcd4a72b6b
donark1 <don_ark@sbcglobal.net>
2021-09-22 14:25:18 -0700

    Adding the beginnings of a modal

---
+++ main.js
@@ -0,0 +1,7 @@
+
+var $addEntryModal = document.querySelector('.addEntryModal');
+var $addEntryButton = document.querySelector('.addEntryButton');
+
+$addEntryButton.onclick = function () {
+  $addEntryModal.style.display = 'block';
+};
